7f63322c526e5949eb21fb49ab223a98841434fa
sedrck <84310764+sedrck@users.noreply.github.com>
2024-09-26 01:09:06 +0800

    Update on visitorcounter.js and IaC implementation

--- website/assets/js/visitorcounter.js
+++ website/assets/js/visitorcounter.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://swlf3akfd6zh3d42g75z7pmfza0jisok.lambda-url.ap-southeast-2.on.aws/");
+    let response = await fetch("https://mwbc5f46uohj6h4yk2heyqww7q0gknpv.lambda-url.ap-southeast-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = `PAGE VIEWS: ${data}`;
 }
